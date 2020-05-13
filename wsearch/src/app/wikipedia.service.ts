e0e21196df78e4026b92f84640a3503b52c84e3c
Djuro Alfirevic <djalfirevic@gmail.com>
2020-05-13 16:53:15 +0200

    SearchBar component updated

--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class WikipediaService {
-
-  constructor() { }
+  url =
+    "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=";
+  constructor() {}
 }
