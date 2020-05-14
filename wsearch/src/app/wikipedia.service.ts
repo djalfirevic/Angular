77607f7cb96f40300829218a1565862108b43e4a
Djuro Alfirevic <djalfirevic@gmail.com>
2020-05-14 11:18:43 +0200

    Wikipedia service updated

--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class WikipediaService {
-  url =
-    "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=";
   constructor() {}
+
+  public search(term: string) {
+    let url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${term}`;
+  }
 }
