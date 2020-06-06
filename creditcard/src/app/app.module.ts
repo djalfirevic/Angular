840d4a1c18a53833704003405e9314db00d2829e
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-06 08:06:05 +0200

    Adding ngx-mask library

--- creditcard/src/app/app.module.ts
+++ creditcard/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { AppComponent } from "./app.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CardFormComponent } from "./card-form/card-form.component";
 import { InputComponent } from "./input/input.component";
+import { NgxMaskModule, IConfig } from "ngx-mask";
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
   declarations: [AppComponent, CardFormComponent, InputComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
   providers: [],
   bootstrap: [AppComponent],
 })
